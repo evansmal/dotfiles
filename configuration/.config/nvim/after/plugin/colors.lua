839c754e96384c05e9238afab753574783bb681b
Evan Smal <evan.smal@hotmail.com>
2023-02-16 18:27:59 -0500

    Add some icon friendly fonts. Use white in colorscheme

--- configuration/.config/nvim/after/plugin/colors.lua
+++ configuration/.config/nvim/after/plugin/colors.lua
@@ -1,3 +1,14 @@
+require("no-clown-fiesta").setup({
+  transparent = false, -- Enable this to disable the bg color
+  styles = { 
+    comments = {},
+    keywords = {},
+    functions = {fg = "#FFFFFF"},
+    variables = {fg = "#FFFFFF"},
+    type = { bold = true },
+  },
+})
+
 function SetColor()
     vim.cmd [[ set background=dark ]]
     vim.cmd[[colorscheme no-clown-fiesta]]
