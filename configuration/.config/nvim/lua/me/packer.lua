2f269ae08cd788450a220a5a03e056f1083e4bdf
Evan Smal <evan.smal@hotmail.com>
2023-03-09 08:09:42 -0500

    Setup rg and fzf for telescope

--- configuration/.config/nvim/lua/me/packer.lua
+++ configuration/.config/nvim/lua/me/packer.lua
@@ -13,6 +13,7 @@ return require('packer').startup(function(use)
         'nvim-telescope/telescope.nvim', tag = '0.1.1',
         requires = { {'nvim-lua/plenary.nvim'} }
     }
+    use {'nvim-telescope/telescope-fzf-native.nvim', run = 'make' }
 
     use 'aktersnurra/no-clown-fiesta.nvim'
 
