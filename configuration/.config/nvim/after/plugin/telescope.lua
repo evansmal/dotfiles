18985ed94a012feaf99f34211894e987a396bb09
Evan Smal <evan.smal@hotmail.com>
2023-03-09 08:18:17 -0500

    Add mapping for grep_string and git_commit pickers

--- configuration/.config/nvim/after/plugin/telescope.lua
+++ configuration/.config/nvim/after/plugin/telescope.lua
@@ -2,8 +2,10 @@ local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
 vim.keymap.set('n', '<leader>gf', builtin.git_files, {})
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+vim.keymap.set('n', '<leader>gs', builtin.grep_string, {})
 vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+vim.keymap.set('n', '<leader>gc', builtin.git_commits, {})
 
 require('telescope').setup {
   extensions = {
