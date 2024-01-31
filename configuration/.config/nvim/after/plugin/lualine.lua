dacfebf24caf39488c75ca9dd24ff23d8bf2929d
Evan Smal <evan.smal@hotmail.com>
2024-01-31 08:25:57 -0500

    Improve statusline layout and remove icons

--- configuration/.config/nvim/after/plugin/lualine.lua
+++ configuration/.config/nvim/after/plugin/lualine.lua
@@ -1,9 +1,9 @@
 require('lualine').setup {
   options = {
-    icons_enabled = true,
+    icons_enabled = false,
     theme = 'auto',
-    component_separators = { left = '', right = ''},
-    section_separators = { left = '', right = ''},
+    component_separators = { left = '', right = ''},
+    section_separators = { left = '', right = ''},
     disabled_filetypes = {
       statusline = {},
       winbar = {},
@@ -20,7 +20,22 @@ require('lualine').setup {
   sections = {
     lualine_a = {'mode'},
     lualine_b = {'branch', 'diff', 'diagnostics'},
-    lualine_c = {'filename'},
+    lualine_c = 
+    {
+        {
+            'filename',
+            file_status = true,      -- Displays file status (readonly status, modified status)
+            newfile_status = false,  -- Display new file status (new file means no write after created)
+            path = 4,
+            shorting_target = 40,    -- Shortens path to leave 40 spaces in the window
+            symbols = {
+                modified = '[+]',      -- Text to show when the file is modified.
+                readonly = '[-]',      -- Text to show when the file is non-modifiable or readonly.
+                unnamed = '[No Name]', -- Text to show for unnamed buffers.
+                newfile = '[New]',     -- Text to show for newly created file before first write
+            }
+        }
+    },
     lualine_x = {'encoding', 'fileformat', 'filetype'},
     lualine_y = {'progress'},
     lualine_z = {'location'}
