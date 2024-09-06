e0f0b5b3943e6f821b5ec607c2d478537840082c
Evan Smal <evan.smal@hotmail.com>
2024-09-06 08:25:08 -0400

    Fix issue in FZF config

--- configuration/.config/nvim/after/plugin/fzf-lua.lua
+++ configuration/.config/nvim/after/plugin/fzf-lua.lua
@@ -1,5 +1,5 @@
 vim.keymap.set("n", "<leader>p", require('fzf-lua').files, { desc = "Fzf Files" })
-km.set("n", "<leader>b", require("fzf-lua").buffers, { desc = "Fzf Buffers" })
+vim.keymap.set("n", "<leader>b", require("fzf-lua").buffers, { desc = "Fzf Buffers" })
 vim.keymap.set("n", "<leader>f", require("fzf-lua").live_grep, { desc = "Fzf Grep" })
 
 vim.keymap.set("n", "<leader>cj", require("fzf-lua").lsp_definitions, { desc = "Jump to Definition" })
